0b74f92efb6a8db2853b4b2938610f7ebc8ebc81
Ulises Ramirez-Roche <ulisesrmzroche@gmail.com>
2017-03-01 19:22:59 -0600

    build project

--- dist/main.trello-pomodoro.js
+++
@@ -1,9 +0,0 @@
-webpackJsonp([0,1],[
-/* 0 */
-/***/ (function(module, exports) {
-
-alert('hello world!');
-
-
-/***/ })
-],[0]);
---
+++ scripts/build.js
@@ -0,0 +1,8 @@
+// rm -rf tmp dist && webpack --config ./config/webpack.config.js && cp -r lib/* dist
+
+// clean(["tmp", "dist"])
+// .then(()=>{
+//   webpack(webpackConfig)
+// }).then(()=>{
+//   // package extensions into their directories
+// })
